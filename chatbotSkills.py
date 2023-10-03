ed69b17d69a3d330a8cba6026a442c58deec6851
Journey McDowell <jomcdowe@microsoft.com>
2023-10-02 19:12:51 -0700

    Using class with new MSFT earnings transcript format with pdf's and formrecognizer

--- chatbotSkills.py
+++ chatbotSkills.py
@@ -165,4 +165,18 @@ def count_tokens(string: str, encoding_name: str="gpt-4-32k") -> int:
     num_tokens = len(encoding.encode(string))
     return num_tokens
 
+##############################################################
+###### Get Prompt Template from csv  #############
+##############################################################
+
+def get_prompt_template(prompt_id, prompt_templates_name=None):
+    """
+    Retrieve LLM prompt template using prompt_id from a csv file.
+    """
+    if prompt_templates_name == None:
+        prompt_templates_name = config['PROMPT_TEMPLATE_FILE']
+    df = pd.read_csv(os.path.join(os.path.dirname(__file__), prompt_templates_name))
+    prompt = df[df['prompt_id'] == prompt_id]['prompt_template'].values[0]
+    return prompt
+
 
